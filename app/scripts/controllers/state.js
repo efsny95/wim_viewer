5f71a397313eb7ce6ddfb5700ddc707bd14eea2b
Evan <efsny@msn.com>
2014-04-24 13:56:18 -0400

    List stations on state page

--- app/scripts/controllers/state.js
+++ app/scripts/controllers/state.js
@@ -1,9 +1,30 @@
 'use strict';
 
 angular.module('wimViewerApp')
-  .controller('StateCtrl', function ($scope,$routeParams) {
+  .controller('StateCtrl', function ($scope,$http,$routeParams) {
   	$scope.stateFips = $routeParams['stateFips']
-  	$http.get('http://localhost:1337/stations/byState/'+stateFips).success(function(data, status, headers, config) {
-  		console.log(data);
+  	$scope.stations = []
+ 	//var count = 0
+  	$http.get('http://localhost:1337/stations/byState/'+$scope.stateFips).success(function(data, status, headers, config) {
+  		//console.log($scope.stateFips);
+  		data.rows.forEach(function(row){
+  			/*var stateFip = row.f[0].v;
+  			var stationId = row.f[1].v;
+  			var year = row.f[2].v;
+  			var month = row.f[3].v;
+  			var numTrucks = row.f[4].v;
+  			console.log(row);*/
+  			var rowStation = row.f[1].v;
+  			//$scope.stations.push({'stationId':row.f[1].v,'year':row.f[2].v,'months':row.f[3].v,'numTrucks':row.f[4].v});
+  			
+  			if(getStationIndex(rowStation) == -1) {
+  				$scope.stations.push({'stationId':rowStation, months:[]})
+  			}
+  			$scope.stations[getStationIndex(rowStation)].months.push({'year':row.f[2].v,'month':row.f[3].v,'numTrucks':row.f[4].v});
+  			//count++;
+  		}); 
   	})
+  	function getStationIndex(stationID){
+  		return $scope.stations.map(function(el) {return el.stationId;}).indexOf(stationID);
+  	}
   });
