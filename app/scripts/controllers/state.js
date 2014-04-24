ca10b4513cc7a1111a46cd9f53d30949426584be
Evan <efsny@msn.com>
2014-04-24 15:26:58 -0400

    Update to states view thumbnail

--- app/scripts/controllers/state.js
+++ app/scripts/controllers/state.js
@@ -12,15 +12,15 @@ angular.module('wimViewerApp')
   			var stationId = row.f[1].v;
   			var year = row.f[2].v;
   			var month = row.f[3].v;
-  			var numTrucks = row.f[4].v;
-  			console.log(row);*/
-  			var rowStation = row.f[1].v;
+  			var numTrucks = row.f[4].v;*/
+  			//console.log(row);
+  			var rowStation = row.f[0].v;
   			//$scope.stations.push({'stationId':row.f[1].v,'year':row.f[2].v,'months':row.f[3].v,'numTrucks':row.f[4].v});
   			
   			if(getStationIndex(rowStation) == -1) {
-  				$scope.stations.push({'stationId':rowStation, months:[]})
+  				$scope.stations.push({'stationId':rowStation, years:[]})
   			}
-  			$scope.stations[getStationIndex(rowStation)].months.push({'year':row.f[2].v,'month':row.f[3].v,'numTrucks':row.f[4].v});
+  			$scope.stations[getStationIndex(rowStation)].years.push({'year':row.f[1].v,'percent':(row.f[4].v)*100,'AADT':Math.round(row.f[5].v)});
   			//count++;
   		}); 
   	})
