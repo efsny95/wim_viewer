212e41e753743e2f06029e54ab817227e89d9ca3
Eric Conklin <eric.s.conklin@gmail.com>
2014-06-16 12:14:00 -0400

    some stuff

--- app/scripts/src/wimXHR.js
+++ app/scripts/src/wimXHR.js
@@ -1,24 +1,33 @@
 (function() {
 	var wimXHR = {
-		version: '0.2.0'
+		version: '0.2.1'
 	}
-
+	// variable to hold the database name that the client
+	// will be querying from
 	var _DATABASE = null;
 
-	var URLbase = 'http://localhost:1337/' //'http://api.availabs.org/wim/'
+	// domain where API is located
+	var _URLbase = 'http://localhost:1337/';
 
+	// AJAX get request convenience method
+	// simple redirects to the post method
 	wimXHR.get = function(url, callback) {
 		wimXHR.post(url, {database: _DATABASE}, callback);
 	}
 
+	// AJAX post request
+	// callback is optional
+	// url should not contain a domain name, just a route
+	// data shoud be contained within a json
 	wimXHR.post = function(url, data, callback) {
 		if (/^\//.test(url)) {
 			url = url.slice(1);
 		}
 		data.database = _DATABASE;
-		_getXHR(URLbase+url).post(JSON.stringify(data), function(error, data) {
-													callback(error, data);
-												});
+		_getXHR(_URLbase+url).post(JSON.stringify(data), function(error, data) {
+			if (typeof callback !== undefined)
+				callback(error, data);
+		});
 	}
 
 	function _getXHR(url) {
@@ -26,13 +35,14 @@
                     return JSON.parse(request.responseText);
                 })
 	}
-
+	// retrieves client database name from file 'session.conf'
 	d3.json('./session.conf', function(error, config) {
         if (error) {
           console.log('Could not load config file', error);
           return;
         }
         _DATABASE = config.database;
+        _URLbase = _URLbase || config.urlbase;
     })
 
 	this.wimXHR = wimXHR;
